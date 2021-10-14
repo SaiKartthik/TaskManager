82de6800487a931d40e7b729e974f3f30899f74e
saikarthhik <71244295+saikarthhik@users.noreply.github.com>
2021-10-14 11:51:31 +0530

    Clean

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const task_container = document.querySelector("#task_container")
 let globalTaskData = [];
+
+const generateHTML = (taskData) =>{return`	<div id = ${(taskData.id)} class=" col-md-6 col-lg-4 ">
+  <div class="card">
+    <div class="card-header gap-2 d-flex justify-content-end ">
+      <button style="border" class="btn btn-outline-info  " type="button" name="button">
+        <i class="fal fa-pencil"></i></button>
+      <button class="btn btn-outline-danger " type="button" name="button">
+        <i class="fal fa-trash-alt"></i></button>
+    </div>
+    <div class="card-body ">
+      <img class="img-fluid" src=${(taskData.image)}>
+      <h5 class="card-title mt-4">${(taskData.title)}</h5>
+      <p class="card-text">${(taskData.Description)}</p>
+      <span class="badge bg-primary">${(taskData.type)}</span>
+    </div>
+    <div class="card-footer ">
+      <button class="btn btn-outline-primary" type="button" name="button">Open Task</button>
+    </div>
+  </div>
+</div>`
+}
+
 const addNewCard = () => {
   //get task data
   const taskData = {
@@ -14,25 +36,7 @@ globalTaskData.push(taskData);
 // var cards = globalTaskData
 localStorage.setItem("TaskyKey",JSON.stringify({card : globalTaskData}));
   //create card
-  const newcard = `	<div id = ${(taskData.id)} class=" col-md-6 col-lg-4 ">
-    <div class="card">
-      <div class="card-header gap-2 d-flex justify-content-end ">
-        <button style="border" class="btn btn-outline-info  " type="button" name="button">
-          <i class="fal fa-pencil"></i></button>
-        <button class="btn btn-outline-danger " type="button" name="button">
-          <i class="fal fa-trash-alt"></i></button>
-      </div>
-      <div class="card-body ">
-        <img class="img-fluid" src=${(taskData.image)}>
-        <h5 class="card-title mt-4">${(taskData.title)}</h5>
-        <p class="card-text">${(taskData.Description)}</p>
-        <span class="badge bg-primary">${(taskData.type)}</span>
-      </div>
-      <div class="card-footer ">
-        <button class="btn btn-outline-primary" type="button" name="button">Open Task</button>
-      </div>
-    </div>
-  </div>`
+  const newcard = generateHTML(taskData)
 
   //inject to DOM
 
@@ -61,26 +65,7 @@ const loadExistingCards = () =>{
   //inject it to DOM
 
   globalTaskData.map((taskData) => {
-      const newcard = `	<div id = ${(taskData.id)} class=" col-md-6 col-lg-4 ">
-        <div class="card">
-          <div class="card-header gap-2 d-flex justify-content-end ">
-            <button style="border" class="btn btn-outline-info  " type="button" name="button">
-              <i class="fal fa-pencil"></i></button>
-            <button class="btn btn-outline-danger " type="button" name="button">
-              <i class="fal fa-trash-alt"></i></button>
-          </div>
-          <div class="card-body ">
-            <img class="img-fluid" src=${(taskData.image)}>
-            <h5 class="card-title mt-4">${(taskData.title)}</h5>
-            <p class="card-text">${(taskData.Description)}</p>
-            <span class="badge bg-primary">${(taskData.type)}</span>
-          </div>
-          <div class="card-footer ">
-            <button class="btn btn-outline-primary" type="button" name="button">Open Task</button>
-          </div>
-        </div>
-      </div>`
-
+      const newcard = generateHTML(taskData)
         task_container.insertAdjacentHTML("beforeend", newcard);
 
   })
